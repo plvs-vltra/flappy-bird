99de6ff4b675296b670a0df2c2776555ba688c77
plvs vltra <justemailpostbox@gmail.com>
2022-07-17 00:18:07 +0500

    Basic game JS classes

---
+++ js/game.js
@@ -0,0 +1,33 @@
+class World{
+    constructor(){
+
+    }
+
+    render(){
+
+    }
+}
+
+
+
+class WorldObject{
+    constructor(){
+
+    }
+}
+
+
+
+class Tube extends WorldObject{
+    constructor(){
+
+    }
+}
+
+
+
+class Bird extends WorldObject{
+    constructor(){
+
+    }
+}
